040d4ba1a0e16f47964daab72e46f7970ee02f78
Gonzalo Cáceres <47033752+GonzaloMCG@users.noreply.github.com>
2021-04-27 16:15:20 -0300

    errorpage
    errorpage

--- facturacion-web/src/app/app-routing.module.ts
+++ facturacion-web/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
 import { InvoicingPageComponent } from './landing/invoicing-page/invoicing-page.component';
 import { LandingComponent } from './landing/landing.component';
 import { MenuPanelComponent } from './landing/menu-panel/menu-panel.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   {
     path: 'landing', component: LandingComponent,
     children: [
@@ -14,7 +17,8 @@ const routes: Routes = [
       { path: 'invoices-emitted-page', component: LandingComponent, pathMatch: 'full' },
       { path: 'stack-page', component: LandingComponent, pathMatch: 'full' },
     ],
-  }
+  },
+  { path: '**', component: ErrorComponent }
 ];
 
 @NgModule({
