bebf5d4ec608a92f89e3ec3c1482022ab2767316
Gonzalo Cáceres <47033752+GonzaloMCG@users.noreply.github.com>
2021-04-30 17:35:12 -0300

    Resto de las ventanas
    Resto de las ventanas

--- facturacion-web/src/app/app-routing.module.ts
+++ facturacion-web/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
+import { InvoicesEmittedComponent } from './landing/invoices-emitted-page/invoices-emitted-page.component';
 import { InvoicingPageComponent } from './landing/invoicing-page/invoicing-page.component';
 import { LandingComponent } from './landing/landing.component';
 import { MenuPanelComponent } from './landing/menu-panel/menu-panel.component';
+import { StockPageComponent } from './landing/stock-page/stock-page.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
@@ -14,8 +16,8 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home-page', pathMatch: 'full' },
       { path: 'home-page', component: InvoicingPageComponent, pathMatch: 'full' },
-      { path: 'invoices-emitted-page', component: LandingComponent, pathMatch: 'full' },
-      { path: 'stack-page', component: LandingComponent, pathMatch: 'full' },
+      { path: 'invoices-emitted-page', component: InvoicesEmittedComponent, pathMatch: 'full' },
+      { path: 'stock-page', component: StockPageComponent, pathMatch: 'full' },
     ],
   },
   { path: '**', component: ErrorComponent }
