735f1ac2f9c23d15f38c924ba692576715d3d121
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-05-02 19:03:17 -0300

    wip: table

--- facturacion-web/src/app/landing/landing.module.ts
+++ facturacion-web/src/app/landing/landing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule } from '@angular/router';
 import { InvoicingPageComponent } from './invoicing-page/invoicing-page.component';
 import { LandingComponent } from './landing.component';
 import { MenuPanelComponent } from './menu-panel/menu-panel.component';
+import { CdkTableModule } from '@angular/cdk/table';
 
 
 @NgModule({
   declarations: [
     LandingComponent,
     MenuPanelComponent,
-    InvoicingPageComponent
+    InvoicingPageComponent,
+    CdkTableModule,
   ],
   exports: [
     LandingComponent
