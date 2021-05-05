aa2cf90b9f3a42aa5b45eecdccdc295d9ce4015a
Gonzalo Cáceres <47033752+GonzaloMCG@users.noreply.github.com>
2021-05-05 02:49:21 -0300

    Todo pronto para mañana
    Todo pronto para mañana

--- facturacion-web/src/app/app-routing.module.ts
+++ facturacion-web/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ErrorComponent } from './error/error.component';
 import { AuthGuard } from './helpers/auth.guard';
+import { InvoicesEmittedComponent } from './landing/invoices-emitted-page/invoices-emitted-page.component';
 import { InvoicingPageComponent } from './landing/invoicing-page/invoicing-page.component';
 import { LandingComponent } from './landing/landing.component';
 import { MenuPanelComponent } from './landing/menu-panel/menu-panel.component';
@@ -15,7 +16,7 @@ const routes: Routes = [
     children: [
       { path: '', redirectTo: 'home-page', pathMatch: 'full' },
       { path: 'home-page', component: InvoicingPageComponent, pathMatch: 'full' },
-      { path: 'invoices-emitted-page', component: LandingComponent, pathMatch: 'full' },
+      { path: 'invoices-emitted-page', component: InvoicesEmittedComponent, pathMatch: 'full' },
     ],
   },
   { path: '**', component: ErrorComponent }
