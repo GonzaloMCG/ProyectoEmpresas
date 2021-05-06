7e89cf32cb3a7d6421ca3e211390b939873ca829
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-05-06 15:41:53 -0300

    example mat dialog

---
+++ facturacion-web/src/app/landing/invoicing-page/example-modal/example-modal.component.ts
@@ -0,0 +1,22 @@
+// import { Component, Inject } from '@angular/core';
+// import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+// @Component({
+//   selector: 'app-example-modal',
+//   templateUrl: 'example-modal.component.html',
+//   styleUrls: ['./example-modal.component.scss']
+// })
+
+// export class ExampleModalComponent {
+//   constructor(public dialogRef: MatDialogRef<ExampleModalComponent>,
+//     @Inject(MAT_DIALOG_DATA) public data: any) { }
+
+//   close() {
+//     this.dialogRef.close();
+//   }
+
+//   submit() {
+//     this.dialogRef.close(true);
+//   }
+// }
