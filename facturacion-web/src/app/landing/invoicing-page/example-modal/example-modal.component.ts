e6bc8478cbb1c4ccd6cb18d007639bad4e3c6d7f
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-05-06 15:58:15 -0300

    implementation

--- facturacion-web/src/app/landing/invoicing-page/example-modal/example-modal.component.ts
+++ facturacion-web/src/app/landing/invoicing-page/example-modal/example-modal.component.ts
@@ -1,22 +1,22 @@
-// import { Component, Inject } from '@angular/core';
-// import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 
-// @Component({
-//   selector: 'app-example-modal',
-//   templateUrl: 'example-modal.component.html',
-//   styleUrls: ['./example-modal.component.scss']
-// })
+@Component({
+  selector: 'app-example-modal',
+  templateUrl: 'example-modal.component.html',
+  styleUrls: ['./example-modal.component.scss']
+})
 
-// export class ExampleModalComponent {
-//   constructor(public dialogRef: MatDialogRef<ExampleModalComponent>,
-//     @Inject(MAT_DIALOG_DATA) public data: any) { }
+export class ExampleModalComponent {
+  constructor(public dialogRef: MatDialogRef<ExampleModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) { }
 
-//   close() {
-//     this.dialogRef.close();
-//   }
+  close() {
+    this.dialogRef.close();
+  }
 
-//   submit() {
-//     this.dialogRef.close(true);
-//   }
-// }
+  submit() {
+    this.dialogRef.close(true);
+  }
+}
