1afe82e13bcdac621d78f2c5eccb3244df0678fa
fp90 <>
2021-05-13 21:38:29 -0300

    new sidebar

--- facturacion-web/src/app/landing/menu-panel/menu-panel.component.ts
+++ facturacion-web/src/app/landing/menu-panel/menu-panel.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import {MatSidenav} from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-menu-panel',
@@ -10,8 +12,22 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 export class MenuPanelComponent {
   public userName = "No Name";
   currentDate = new Date();
+  @ViewChild(MatSidenav)
+  sidenav!: MatSidenav;
 
-  constructor(private authenticationService: AuthenticationService) { }
+  constructor(private authenticationService: AuthenticationService, private observer: BreakpointObserver) { }
+
+  ngAfterViewInit() {
+    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+      if (res.matches) {
+        this.sidenav.mode = 'over';
+        this.sidenav.close();
+      } else {
+        this.sidenav.mode = 'side';
+        this.sidenav.open();
+      }
+    });
+  }
 
   logout() {
     this.authenticationService.logout();
