f202e103964e141b41be98b136ee71fc957662ab
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-05-15 20:56:05 -0300

    error ans succes messages and login error

--- facturacion-web/src/app/login/login.component.ts
+++ facturacion-web/src/app/login/login.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { User } from '../models/user.model';
 import { MessageService } from '../message-handler/message.service';
-import { UserService } from '../services/user.service';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
   selector: 'login',
@@ -21,25 +22,38 @@ export class LoginComponent {
   });
 
   constructor(private formBuilder: FormBuilder,
-    private userService: UserService,
-    private messageService: MessageService) {
+    private messageService: MessageService,
+    private authenticationService: AuthenticationService,
+    private router: Router) {
+
+    // redirect to home if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/landing']);
+    }
   }
 
   onSubmit() {
-    const userData = {
-      user: this.loginForm.get('userName').value,
-      password: this.loginForm.get('userPassword').value,
-    }
-
     try {
-      this.userService.loginUser(userData);
-    } catch (error) {
-      // this.messageService.showError(error);
-    }
-  }
+      this.submitted = true;
 
-  displayMessage() {
-    this.messageService.showSuccess('hola success', 3000);
-    // this.messageService.showError('hola error', 3000);
+      // stop here if form is invalid
+      if (this.loginForm.invalid) {
+        return;
+      }
+
+      this.loading = true;
+      this.authenticationService.login(this.loginForm.get('userName').value,
+        this.loginForm.get('userPassword').value)
+        .subscribe(
+          data => {
+            this.messageService.showSuccess('Ingresado correctamente', 3000);
+            this.router.navigate(['/landing']);
+          },
+          error => {
+            this.error = error;
+            this.loading = false;
+            this.messageService.showError(error, 30333);
+          });
+    } catch (error) { }
   }
 }
