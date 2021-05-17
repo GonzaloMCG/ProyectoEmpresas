c97015feaf3b39318d178c620b69380cf4bba2bb
Gonzalo Cáceres <47033752+GonzaloMCG@users.noreply.github.com>
2021-05-17 11:42:50 -0300

    Update app.component.ts

--- facturacion-web/src/app/app.component.ts
+++ facturacion-web/src/app/app.component.ts
@@ -17,8 +17,6 @@ export class AppComponent {
   ) {
     this.authenticationService.$currentUserSubject.subscribe(x => this.currentUser = x);
   }
-  
-  
   logout() {
     this.authenticationService.logout();
     this.router.navigate(['/login']);
