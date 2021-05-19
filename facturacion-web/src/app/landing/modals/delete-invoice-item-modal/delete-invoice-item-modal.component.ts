3f64a54d208c7991619fd46029da6ba9f9b3eb72
Gonzalo Cáceres <47033752+GonzaloMCG@users.noreply.github.com>
2021-05-19 19:46:43 -0300

    delete-invoice-item-modal
    delete-invoice-item-modal

---
+++ facturacion-web/src/app/landing/modals/delete-invoice-item-modal/delete-invoice-item-modal.component.ts
@@ -0,0 +1,38 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-delete-invoice-item-modal',
+  templateUrl: 'delete-invoice-item-modal.component.html',
+  styleUrls: ['./delete-invoice-item-modal.component.scss']
+})
+
+export class DeleteInvoiceItemModalComponent {
+  public bodyText: string;
+
+  constructor(public dialogRef: MatDialogRef<DeleteInvoiceItemModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  ngOnInit() {
+    this.bodyText = '¿Desea eliminar el producto de la factura?';
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
+
+  submit() {
+    this.removeProduct();
+  }
+
+  async removeProduct() {
+    try {
+      /*QUITAR LA FILA DE LA TABLA*/
+      this.dialogRef.close();
+    } catch (error) {
+      this.dialogRef.close();
+    }
+  }
+}
