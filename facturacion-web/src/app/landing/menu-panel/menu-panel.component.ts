5130764d056392c4f2d899fbd51ee59957c8744c
ceciDucos <58122952+ceciDucos@users.noreply.github.com>
2021-05-30 15:45:20 -0300

    first inputs

--- facturacion-web/src/app/landing/menu-panel/menu-panel.component.ts
+++ facturacion-web/src/app/landing/menu-panel/menu-panel.component.ts
@@ -18,7 +18,7 @@ export class MenuPanelComponent {
   constructor(private authenticationService: AuthenticationService, private observer: BreakpointObserver) { }
 
   ngAfterViewInit() {
-    this.observer.observe(['(max-width: 800px)']).subscribe((res) => {
+    this.observer.observe(['(max-width: 768px)']).subscribe((res) => {
       if (res.matches) {
         this.sidenav.mode = 'over';
         this.sidenav.close();
